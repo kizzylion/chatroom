80afd11f468d5f56439becd839ac064009deae4b
kizzylion <kztchm@gmail.com>
2025-01-26 13:07:25 -0500

    FIX: POOL.JS FILE

--- db/pool.js
+++ db/pool.js
@@ -4,14 +4,31 @@ require("dotenv").config();
 // pg
 const { Pool } = require("pg");
 
-// create pool
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
+
+// create pool
+// const pool = new Pool({
+//   // url: process.env.DATABASE_URL,
+//   host: "ep-snowy-term-a2l62ffx-pooler.eu-central-1.aws.neon.tech",
+//   user: "kizzylion_owner",
+//   password: "6MR7toaGyiwq",
+//   database: "kizzylion",
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
+// });
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT,
+// });
 
 // export pool
 module.exports = pool;
